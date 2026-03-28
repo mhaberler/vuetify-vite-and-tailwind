5b37ea5a85c7addd51379b14980c322f0600c641
Michael Haberler <git@mah.priv.at>
2026-03-28 09:03:43 +0100

    make timeseries responsive

--- src/components/TimeSeries.vue
+++ src/components/TimeSeries.vue
@@ -1,22 +1,37 @@
 <template>
-  <UplotVue :data="data" :options="options" />
+  <div ref="container" style="width: 100%; height: 100%;">
+    <UplotVue v-if="width && height" :data="data" :options="options" />
+  </div>
 </template>
 
 <script setup>
   import UplotVue from 'uplot-vue'
   import 'uplot/dist/uPlot.min.css'
 
-  const options = {
-    width: 600,
-    height: 300,
+  const container = ref(null)
+  const width = ref(0)
+  const height = ref(0)
+
+  const options = computed(() => ({
+    width: width.value,
+    height: height.value,
     series: [
       {},
       { label: 'Line 1', stroke: '#f66' },
     ],
-  }
+  }))
 
   const data = [
     [0, 1, 2, 3, 4, 5], // x
     [10, 25, 15, 35, 20, 40], // y
   ]
+
+  onMounted(() => {
+    const ro = new ResizeObserver(([entry]) => {
+      width.value = Math.floor(entry.contentRect.width)
+      height.value = Math.floor(entry.contentRect.height)
+    })
+    ro.observe(container.value)
+    onUnmounted(() => ro.disconnect())
+  })
 </script>
